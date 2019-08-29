f37032c545b6a99e74b81baf76a392e8cc403b83
atagadtad <aarontagadtad@gmail.com>
2019-08-29 17:48:05 +0000

    finished test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,27 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-// request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-//   const data = JSON.parse(body);
 
-//   console.log("Description: ", data[0]['description']);
-// })
+const fetchBreedDescription = function (breedName, callback) {
 
-request(`https://api.thecatapiasdf.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    process.exit();
-  }
-  const data = JSON.parse(body);
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
 
-  if (data.length === 0) {
-    console.log('Breed not found, please try again');
-    process.exit();
-  } else {
-    console.log(data);
-    // console.log(`Description for ${args[0]}: `, data[0]['description']);
-  }
-});
+      process.exit();
+      // if request function meets an error, it will use fetchBreedDescriptions'
+      // callback function to give it the error
+      callback(error);
+    }
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback(null, '');
+      process.exit();
+    } else {
+      callback(null, data[0]['description']);
+    }
+  });
+};
+
+
+
+module.exports = { fetchBreedDescription };
